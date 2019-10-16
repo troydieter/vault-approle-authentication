d80c53c7c4479590737021ab9f118f28b1217bac
tdieter <tdieter@rgis.com>
2019-10-16 11:35:16 -0400

    Commented out WIP Added example for URL's

--- vault-approle-authentication.py
+++ vault-approle-authentication.py
@@ -1,17 +1,20 @@
 ##########################################################################################################################
 # vault-approle-authentication
 # https://github.com/troydieter/vault-approle-authentication
+# Define secret_id and role_id in the loginJson
+# Set requests.post and get for your Vault API endpoints
 ##########################################################################################################################
 
 import sys
 import requests, getpass
-import json
 
-stored_approle = {
-}
+# Work in progress below
+# import json
+# stored_approle = {
+# }
 
-with open('approle_creds.json', 'w') as json_file:
-    json.dump(stored_approle, json_file)
+# with open('approle_creds.json', 'w') as json_file:
+#    json.dump(stored_approle, json_file)
 
 def getAWS_KEYS():
     try:
@@ -21,12 +24,12 @@ def getAWS_KEYS():
             "secret_id": "",
 	        "role_id": ""
         }
-        r = requests.post("", data={}, json=loginJson, headers=headersLogin)
+        r = requests.post("https://vault-api.contoso.com/v1/auth/approle/login", data={}, json=loginJson, headers=headersLogin)
         data = r.json()
         token = data['auth']["client_token"]
 
         headersKeys = {"X-Vault-Token": token}
-        r = requests.get("", headers=headersKeys)
+        r = requests.get("https://vault-api.contoso.com/v1/aws/creds/approle-name", headers=headersKeys)
         data = r.json()
 
         return data["data"]["access_key"], data["data"]["secret_key"]
